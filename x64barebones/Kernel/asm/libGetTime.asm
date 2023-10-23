483f9c8afb2846553a4c75798d589d87b5d971de
valentinamarti <vmartireta@itba.edu.ar>
2023-10-22 23:27:56 -0300

    Update libGetTime.asm

--- x64barebones/Kernel/asm/libGetTime.asm
+++ x64barebones/Kernel/asm/libGetTime.asm
@@ -1,85 +1,85 @@
-GLOBAL getSeconds
-GLOBAL getMinutes 
-GLOBAL getHours
-GLOBAL getDays
-GLOBAL getMonth
-GLOBAL getYear
+; GLOBAL getSeconds
+; GLOBAL getMinutes 
+; GLOBAL getHours
+; GLOBAL getDays
+; GLOBAL getMonth
+; GLOBAL getYear
 
-section .text 
+; section .text 
 
-getSeconds:
-    push rbp
-    mov rbp, rsp
+; getSeconds:
+;     push rbp
+;     mov rbp, rsp
 
-    mov al, 0
-    out 70h, al
-    in ax, 71h
+;     mov al, 0
+;     out 70h, al
+;     in ax, 71h
    
-    mov rsp, rbp
-    pop rbp
-    ret
+;     mov rsp, rbp
+;     pop rbp
+;     ret
 
-getMinutes:                  
-    push rbp
-    mov rbp, rsp
+; getMinutes:                  
+;     push rbp
+;     mov rbp, rsp
 
-    mov al, 2
-    out 70h, al
-    in ax, 71h
+;     mov al, 2
+;     out 70h, al
+;     in ax, 71h
    
-    mov rsp, rbp
-    pop rbp
-    ret
+;     mov rsp, rbp
+;     pop rbp
+;     ret
 
-getHours:                  
-    push rbp
-    mov rbp, rsp
+; getHours:                  
+;     push rbp
+;     mov rbp, rsp
 
-    mov al, 4
-    out 70h, al
-    in ax, 71h
+;     mov al, 4
+;     out 70h, al
+;     in ax, 71h
    
-    mov rsp, rbp
-    pop rbp
-    ret
+;     mov rsp, rbp
+;     pop rbp
+;     ret
 
 
-getDays:           
-    push rbp
-    mov rbp, rsp
+; getDays:           
+;     push rbp
+;     mov rbp, rsp
 
-    mov al, 6
-    out 70h, al
-    in ax, 71h
+;     mov al, 6
+;     out 70h, al
+;     in ax, 71h
    
-    mov rsp, rbp
-    pop rbp
-    ret
+;     mov rsp, rbp
+;     pop rbp
+;     ret
 
-getMonth:          
-    push rbp
-    mov rbp, rsp
+; getMonth:          
+;     push rbp
+;     mov rbp, rsp
 
-    mov al, 8
-    out 70h, al
-    in ax, 71h
+;     mov al, 8
+;     out 70h, al
+;     in ax, 71h
    
-    mov rsp, rbp
-    pop rbp
-    ret
+;     mov rsp, rbp
+;     pop rbp
+;     ret
 
 
-getYear:      
-    push rbp
-    mov rbp, rsp
+; getYear:      
+;     push rbp
+;     mov rbp, rsp
 
-    mov al, 9
-    out 70h, al
-    in ax, 71h
+;     mov al, 9
+;     out 70h, al
+;     in ax, 71h
    
-    mov rsp, rbp
-    pop rbp
-    ret
+;     mov rsp, rbp
+;     pop rbp
+;     ret
 
-section .data 
-section .bss
+; section .data 
+; section .bss
