a7987d9a2024555f3f2625b7871d78842eb7d13b
valentinamarti <vmartireta@itba.edu.ar>
2023-10-22 23:27:54 -0300

    Update libasm.asm

--- x64barebones/Kernel/asm/libasm.asm
+++ x64barebones/Kernel/asm/libasm.asm
@@ -1,6 +1,13 @@
 GLOBAL cpuVendor
 GLOBAL getKey
 GLOBAL getKeyPressed
+GLOBAL getSeconds
+GLOBAL getMinutes 
+GLOBAL getHours
+GLOBAL getDays
+GLOBAL getMonth
+GLOBAL getYear
+GLOBAL bcdToDecimal
 
 section .text
 	
@@ -28,19 +35,22 @@ cpuVendor:
 	pop rbp
 	ret
 
-getKey:
-    enter 0,0
-.wait:
-    xor rax,rax
+;--------------------------------------------
+; Esta me parece que no la vamos a usar
+;--------------------------------------------
+; getKey:
+;     enter 0,0
+; .wait:
+;     xor rax,rax
 
-    in al, 0x64
-    test al, 1
-    jz .wait
+;     in al, 0x64
+;     test al, 1
+;     jz .wait
 
-    in al, 0x60
+;     in al, 0x60
 
-    leave
-    ret
+;     leave
+;     ret
 
 getKeyPressed:
     push rbp
@@ -52,6 +62,63 @@ getKeyPressed:
     mov rsp, rbp
     pop rbp
     ret
+
+getSeconds:
+    push rbp
+    mov rbp, rsp
+
+    mov al, 0
+    out 70h, al
+    in ax, 71h
+    call bcdToDecimal
+   
+    mov rsp, rbp
+    pop rbp
+    ret
+
+getMinutes:                  
+    push rbp
+    mov rbp, rsp
+
+    mov al, 2
+    out 70h, al
+    in ax, 71h
+    call bcdToDecimal
+   
+    mov rsp, rbp
+    pop rbp
+    ret
+
+getHours:                  
+    push rbp
+    mov rbp, rsp
+
+    mov al, 4
+    out 70h, al
+    in ax, 71h
+    call bcdToDecimal
+   
+    mov rsp, rbp
+    pop rbp
+    ret
+
+bcdToDecimal:	; probar si funciona
+	push rbx
+
+	mov rbx, 0
+	mov bl, al
+	shr al, 4
+	mov bh, 10
+	mul bh
+	and bl, 0fh
+	add al, bl
+
+	pop rbx
+	ret    
+
+
+section .data 
+section .bss
 
 
 
