dc15dd7dbd7be7c273405c5854aa2657509a2d3b
valentinamarti <vmartireta@itba.edu.ar>
2023-11-05 19:04:25 -0300

    commit registerdump

--- x64barebones/Kernel/drivers/include/registers.h
+++ x64barebones/Kernel/drivers/include/registers.h
@@ -6,6 +6,8 @@
 void saveRegisters(uint64_t rsi, uint64_t rdx, uint64_t rcx);
 void genericException(char* message, int len, uint64_t *ripaux, uint64_t *rspaux, uint64_t *stackaux);
 static void genericMemoryDump(char* message, int len);
+static void saveRegsInfo();
 static void printRegisters();
+
 
 #endif
