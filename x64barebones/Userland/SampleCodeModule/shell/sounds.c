fffadf3f0c5dc996688282b50ecf21c4e9546b60
valentinamarti <vmartireta@itba.edu.ar>
2023-11-07 21:19:55 -0300

    Update sounds.c

--- x64barebones/Userland/SampleCodeModule/shell/sounds.c
+++ x64barebones/Userland/SampleCodeModule/shell/sounds.c
@@ -3,24 +3,25 @@
 extern void do_sys_play_beep(int frec,int millisec);
 
 void playWinnerSound(){
-    do_sys_play_beep(330,300);
-    do_sys_play_beep(330,800);
-    do_sys_play_beep(294,300);
-    do_sys_play_beep(294,500);
-    do_sys_play_beep(350,300);
-    do_sys_play_beep(330,300);
-    do_sys_play_beep(330,100);
-    do_sys_play_beep(330,100);
-    do_sys_play_beep(261,100);
-    do_sys_play_beep(330,300);
-    do_sys_play_beep(330,500);
+    do_sys_play_beep(330,300); //mu
+    do_sys_play_beep(330,800); //chaaaaaa
+    do_sys_play_beep(294,300); //chos
+    do_sys_play_beep(294,250); //ahora
+    do_sys_play_beep(350,300); //nos
+    do_sys_play_beep(330,300); //vimos
+    do_sys_play_beep(261,200); //a
+    do_sys_play_beep(330,300); //ilusio
+    do_sys_play_beep(330,700); //naarrr
 }
 
 void playLoserSound(){
-    do_sys_play_beep(880,3);
-    do_sys_play_beep(780,3);
-    do_sys_play_beep(660,3);
-    do_sys_play_beep(585,6);
-    do_sys_play_beep(780,3);
-    do_sys_play_beep(520,6);
+    do_sys_play_beep(100,500);
+    do_sys_play_beep(70,600);
+    do_sys_play_beep(40,1500);
+
+}
+
+void playSoundEats(){
+    do_sys_play_beep(600,100);
+    do_sys_play_beep(400,100);
 }
