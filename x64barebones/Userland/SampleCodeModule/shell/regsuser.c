59c597697b93964d668460f1d841eedad73c05c8
valentinamarti <vmartireta@itba.edu.ar>
2023-11-09 00:43:31 -0300

    Update regsuser.c

--- x64barebones/Userland/SampleCodeModule/shell/regsuser.c
+++ x64barebones/Userland/SampleCodeModule/shell/regsuser.c
@@ -15,7 +15,7 @@
 #define WIDTHE 500
 #define HEIGHTE 100
 
-static char *regsNames[] = {"RAX <> ", "RBX <> ", "RCX <> ", "RDX <> ", "RSI <> ", "RDI <> ", "RBP <> ", "RSP <> ", "R8  <> ", "R9  <> ", "R10 <> ", "R11 <> ", "R12 <> ", "R13 <> ", "R14 <> ", "R15 <> ", "RIP <> "};
+static char *regsNames[] = {"RAX <> ", "RBX <> ", "RCX <> ", "RDX <> ", "RBP <> ", "RDI <> ", "RSI <> ", "RSP <> ", "R8  <> ", "R9  <> ", "R10 <> ", "R11 <> ", "R12 <> ", "R13 <> ", "R14 <> ", "R15 <> ", "RIP <> "};
 static int container_id;
 
 void run_regsuser(){
